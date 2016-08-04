d2d1972635fc75b462ac97211b356f225af0b469
joe9 <joe9mail@gmail.com>
2016-08-03 23:24:11 -0700

    work in progress of the main procesing loop

--- src/Network/Betfair/API/Main.hs
+++ src/Network/Betfair/API/Main.hs
@@ -31,7 +31,7 @@ import Network.Betfair.Responses.ConnectionMessage
 import Network.Betfair.Responses.MarketChangeMessage
 import Network.Betfair.Responses.OrderChangeMessage
 import Network.Betfair.Responses.StatusMessage
-import Network.Betfair.API.NonBlockingReadFromTChan
+import Network.Betfair.API.ReadFromTChan
 
 main :: IO ()
 main = do
@@ -46,36 +46,65 @@ readerThread chan = do
         putStrLn newInt
         readerThread chan
 
-application :: Context -> [MarketId] -> IO ()
-application context [] =
-  putStrLn "No Market Ids provided, hence, not connecting"
-application context mids =
-  do connection <- connectToBetfair
-     (token,_,loggedOutput) <-
-       runRWST applicationLoop
-               context{cConnection = connection}
-               (def :: StreamingState) {ssAppKey = cAppKey context
-                                       ,ssConnectionState = NotAuthenticated
-                                       ,ssSessionToken = "SESSIONTOKEN"
-                                       ,ssMarkets =
-                                          (Map.fromList .
-                                           map (\mid ->
-                                                  (mid,def {msMarketId = mid}))) mids}
---      putStrLn loggedOutput
-     -- add the below close to signal handler, to close the connection on
-     -- interrupt, etc
-     log (cWriteLogChannel context) "Closing connection"
-     connectionClose connection
+entry
+  betfairProcessingLoop
+    (def :: StreamingState) {ssAppKey = cAppKey context
+                            ,ssConnectionState = NotAuthenticated
+                            ,ssSessionToken = "SESSIONTOKEN"}
 
-applicationLoop
-  :: RWST Context () StreamingState IO ()
-applicationLoop =
+betfairProcessingLoop :: Context -> StreamingState -> IO ()
+betfairProcessingLoop context ss =
+--   blocking read for MarketId's
+  mids <- readMarketIdsFromTChan (cReadMarketIdsChannel context))
+--   start processing those marketids and market ids from streaming state
+  newSs <- betfairProcessing ( ss {ssMarkets = (Map.fromList . map (\mid -> (mid,def {msMarketId = mid}))) mids})
+  when newState.ssConnectionState /= AuthenticationFailed
+            betfairProcessingLoop context ss
+
+betfairProcessing :: Context -> [MarketId] -> IO ()
+betfairProcessing context [] =
+  putStrLn "No Market Ids provided, hence, not connecting"
+betfairProcessing ss
+--  if there are no market ids to process, get out
+  | ssMarkets ss == null = return ss
+-- start processing if there are any marketid's in streaming state
+  | otherwise =
+     result <- try connectToBetfair :: IO (Either SomeException Connection)
+     case result of
+       Left ex -> putStrLn ("Caught exception" ++ show ex) >> threadDelayInSeconds 60 >> betfarProcessing ss
+       Right connection -> do
+        newState <- finally (fmap (\(_,s,_) -> s)
+                              (runRWST authenticateAndReadDataLoop
+                                    context{cConnection = connection} ss))
+                           (log (cWriteLogChannel context) "Closing connection"
+                              >> connectionClose connection)
+        when newState.ssConnectionState /= AuthenticationFailed
+             betfairProcessing newState
+
+authenticateAndReadDataLoop :: RWST Context () StreamingState IO ()
+authenticateAndReadDataLoop =
+updateDataloop : non blocking read for additional market id's
+  if all markets are closed, get out
   do response
      ss <- get
      checkAuthentication (ssConnectionState ss)
+     -- check status and ensure that the authentication was successful
      _ <- response
+     if authenticated, go to readData loop
+     else error out
      return ()
 --      applicationLoop
+
+readDataLoop :: RWST Context () StreamingState IO ()
+readDataLoop =
+  if all markets are closed, get out
+  do
+     updateDataloop : non blocking read for additional market id's
+     write state if changed
+     send subscribe requests if needed
+     response
+     write response to cWriteResponseChannel
+     readDataLoop
 
 connectToBetfair :: IO Connection
 connectToBetfair =
