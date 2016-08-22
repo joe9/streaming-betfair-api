95ff94fd3d6d8f51f3fbbc57fca03865be1c4498
joe9 <joe9mail@gmail.com>
2016-08-22 14:49:30 -0700

    added sample usage

---
+++ sample/Sample.hs
@@ -0,0 +1,20 @@
+{-# LANGUAGE FlexibleContexts  #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+module Sample where
+
+import BasicPrelude
+import Data.Default
+--
+import           Betfair.BulkStreamingAPI
+
+--
+-- app key from betfair subscription
+-- session token from the api
+main :: IO ()
+main = void (stream (undefined :: AppKey) (undefined :: SessionToken))
+
+stream
+  :: AppKey -> SessionToken -> IO StreamingState
+stream a = fmap cState . startStreaming def . initializeContext a
