ed46f75968c944d7f9e218d32d9661368aa52d00
joe9 <joe9mail@gmail.com>
2016-11-26 08:07:40 -0700

    derive Generic and Pretty instances for Microsecond

--- src/TimeUnitsJSONInstance.hs
+++ src/TimeUnitsJSONInstance.hs
@@ -2,12 +2,22 @@
 {-# LANGUAGE FlexibleContexts  #-}
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE TemplateHaskell   #-}
+{-# LANGUAGE StandaloneDeriving   #-}
+{-# LANGUAGE DeriveGeneric   #-}
+{-# LANGUAGE DeriveAnyClass   #-}
 
 module TimeUnitsJSONInstance where
 
 import Data.Aeson.TH   (Options (omitNothingFields), defaultOptions,
                         deriveJSON)
 import Data.Time.Units
+import GHC.Generics
 import Protolude       hiding (FilePath)
 
+import Text.PrettyPrint.GenericPretty
+
 $(deriveJSON defaultOptions {omitNothingFields = True} ''Microsecond)
+
+deriving instance Generic Microsecond
+deriving instance Pretty Microsecond
+
